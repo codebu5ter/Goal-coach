99737701892e9c1ad18673df2598090f694b0209
Nikhil Nayak <iamnikhilnayak@outlook.com>
2017-11-10 12:54:50 +0530

    Add Redux components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firebaseApp } from '../firebase';
 
 class App extends Component {
+  signOut() {
+    firebaseApp.auth().signOut();
+  }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        App
+        <button
+          className="btn btn-danger"
+          onClick={() => this.signOut()}
+        >
+          Sign Out
+        </button>
+      </div>
     )
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log(user);
+  return {}
+}
+
+export default connect(mapStateToProps, null)(App);
